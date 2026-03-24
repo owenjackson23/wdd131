a0a14dc6c8a2f28946df25eef7a1735f9163428c
owenjackson23 <owenryanjackson@icloud.com>
2026-03-23 20:06:23 -0700

    w02 bom activity js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,29 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+// Add chapter
+button.addEventListener("click", function () {
+    if (input.value.trim() !== "") {
+        // Create list item
+        const li = document.createElement("li");
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "❌";
+        // Delete chapter
+        deletebutton.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        // Add delete button to list item
+        li.appendChild(deleteButton);
+        // Add list item to list
+        list.appendChild(li);
+
+        // Clear input
+        input.value = "";
+    }
+    input.focus();
+});
