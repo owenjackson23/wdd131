d2f9e46e3fe41479a8470605a8d178be6c6e4c35
owenjackson23 <owenryanjackson@icloud.com>
2026-03-29 13:10:04 -0700

    finished w03 country page assignment

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,3 +1,4 @@
+// Date and Time
 const year = document.querySelector('#currentyear');
 const lastMod = document.querySelector('#lastModified');
 
@@ -6,6 +7,26 @@ const today = new Date();
 year.innerHTML = today.getFullYear();
 lastMod.innerHTML = document.lastModified
 
+// Data
+const area = document.querySelector('#area');
+const popl = document.querySelector('#population');
+const capl = document.querySelector('#capital');
+const langs = document.querySelector('#languages');
+const curr = document.querySelector('#currency');
+const timeZ = document.querySelector('#timeZone');
+const areaCode = document.querySelector('#areaCode');
+const netTLD = document.querySelector('#internetTLD');
+
+area.innerHTML = 42;
+popl.innerHTML = 159000;
+capl.innerHTML = "Magic Kingdom";
+langs.innerHTML = "English";
+curr.innerHTML = "USD";
+timeZ.innerHTML = "Eastern Time";
+areaCode.innerHTML = "+1(407)";
+netTLD.innerHTML = ".us";
+
+// Weather
 const temp = document.querySelector('#temperature');
 const cond = document.querySelector('#conditions');
 const windSpeed = document.querySelector('#wind');
